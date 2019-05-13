13cb8cc297a03ff3e0234fd60d1ae6e0cc8ef8d4
Alexander <a.karamushko@supportix.ru>
2019-05-13 23:47:36 +0400

    added lazy loading

--- js/es5/script.js
+++ js/es5/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-$(document).ready(function () {
-  $(function () {
+$(document).ready(function() {
+  $(function() {
     $('[data-toggle="tooltip"]').tooltip({
       delay: {
         show: 200,
@@ -10,7 +10,7 @@ $(document).ready(function () {
     });
   });
 
-  window.onscroll = function () {
+  window.onscroll = function() {
     if ($(document).scrollTop() > 80) {
       $("header").addClass("header_scrolled");
     } else {
@@ -20,3 +20,53 @@ $(document).ready(function () {
 
   objectFitImages();
 });
+
+var lazy = [];
+
+registerListener("load", setLazy);
+registerListener("load", lazyLoad);
+registerListener("scroll", lazyLoad);
+registerListener("resize", lazyLoad);
+
+function setLazy() {
+  lazy = document.getElementsByClassName("lazy");
+}
+
+function lazyLoad() {
+  for (var i = 0; i < lazy.length; i++) {
+    if (isInViewport(lazy[i])) {
+      if (lazy[i].getAttribute("data-src")) {
+        lazy[i].src = lazy[i].getAttribute("data-src");
+        lazy[i].classList.add("img-loaded-js");
+        lazy[i].removeAttribute("data-src");
+      }
+    }
+  }
+
+  cleanLazy();
+}
+
+function cleanLazy() {
+  lazy = Array.prototype.filter.call(lazy, function(l) {
+    return l.getAttribute("data-src");
+  });
+}
+
+function isInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.bottom >= 0 &&
+    rect.right >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function registerListener(event, func) {
+  if (window.addEventListener) {
+    window.addEventListener(event, func);
+  } else {
+    window.attachEvent("on" + event, func);
+  }
+}
